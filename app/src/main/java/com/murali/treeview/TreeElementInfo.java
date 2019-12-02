6a11b5fe41a8173db69583d80db7d214c143209d
Murali <mohanmca009@gamil.com>
2019-12-02 08:00:29 +0530

    TreeView - For Expandable ListView

---
+++ app/src/main/java/com/murali/treeview/TreeElementInfo.java
@@ -0,0 +1,23 @@
+package com.murali.treeview;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public interface TreeElementInfo extends Serializable {
+    public void addChild(TreeElementInfo child);
+    public String getId();
+    public void setId(String id);
+    public String getOutlineTitle();
+    public void setOutlineTitle(String outlineTitle);
+    public boolean isHasParent();
+    public void setHasParent(boolean hasParent);
+    public boolean isHasChild();
+    public void setHasChild(boolean hasChild);
+    public int getLevel();
+    public void setLevel(int level);
+    public boolean isExpanded();
+    public void setExpanded(boolean expanded);
+    public ArrayList<TreeElementInfo> getChildList();
+    public TreeElementInfo getParent();
+    public void setParent(TreeElementInfo parent);
+}
---
+++ app/src/main/java/com/murali/treeview/TreeViewActivity.java
@@ -0,0 +1,73 @@
+package com.murali.treeview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ExpandableListView;
+
+import com.google.gson.Gson;
+import com.murali.models.FilterChild;
+import com.murali.models.FilterElements;
+import com.murali.models.FilterSerViceResponse;
+import com.murali.multilevel.ParentLevelListAdapter;
+import com.murali.multilevel.R;
+import com.murali.nlevel.Constants;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//https://stackoverflow.com/questions/18717155/expandablelistview-like-treeview-android/20518038
+https://codeday.me/es/qa/20190702/969528.html
+public class TreeViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TreeViewAdapter mTreeViewAdapter = new TreeViewAdapter();
+    }
+
+
+        private void getFilterResponse() {
+        FilterSerViceResponse responseData= null;
+        List<FilterElements> elementsList = null;
+        String response = Constants.newJsonStringList;
+        try {
+            responseData = new Gson().fromJson(response, FilterSerViceResponse.class);
+        } catch (Exception e) {
+            Log.e("", e.getMessage());
+        }
+        if (null != responseData) {
+            List<FilterSerViceResponse.FilterFacet> filterFacets = responseData.getProducts().getFacets();
+            for (FilterSerViceResponse.FilterFacet mFilterFacet : filterFacets){
+                if( mFilterFacet.getKey().equalsIgnoreCase("categories")){
+                    elementsList  = mFilterFacet.getElements();
+                    for(int i=0; i<elementsList.size();i++){
+
+                        List<FilterChild> child = elementsList.get(i).getChild();
+                        String[] scData = new String[child.size()];
+                        for(int c =0; c<child.size();c++){
+                            scData[c] = child.get(c).getText();
+                            List<FilterChild.SubChild> subChildList =  child.get(c).getSubChild();
+                            if(subChildList!=null && !subChildList.isEmpty()) {
+                                String[] subChildArray = new String[subChildList.size()];
+                                for (int sc = 0; sc < subChildList.size(); sc++) {
+                                    subChildArray[sc] = subChildList.get(sc).getText();
+                                }
+
+                            }
+                        }
+
+
+
+                    }
+
+
+
+                    return;
+                }
+            }
+        }
+    }
+}
