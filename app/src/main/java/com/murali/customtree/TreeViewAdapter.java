fd9eea839f0d91fd595055e02aa995da9f6694d5
muralimohanarao_r <muralimohanarao.ramarao@photoninfotech.net>
2019-12-26 18:08:54 +0530

    Resolved Compilation Issue.

--- app/src/main/java/com/murali/customtree/TreeViewAdapter.java
+++ app/src/main/java/com/murali/customtree/TreeViewAdapter.java
@@ -1,9 +1,6 @@
 package com.murali.customtree;
 
 
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,21 +11,28 @@ import android.widget.TextView;
 import com.murali.multilevel.R;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * TreeViewAdapter
- * @author carrey
  *
+ * @author carrey
  */
 public class TreeViewAdapter extends BaseAdapter {
-    /** Elemental data sources*/
+    /**
+     * Elemental data sources
+     */
     private ArrayList<Element> elementsData;
-    /** Elements in trees*/
+    /**
+     * Elements in trees
+     */
     private ArrayList<Element> elements;
-    /** LayoutInflater */
+    /**
+     * LayoutInflater
+     */
     private LayoutInflater inflater;
-    /** item Head indentation cardinality*/
+    /**
+     * item Head indentation cardinality
+     */
     private int indentionBase;
 
     public TreeViewAdapter(ArrayList<Element> elements, ArrayList<Element> elementsData, LayoutInflater inflater) {
@@ -69,6 +73,8 @@ public class TreeViewAdapter extends BaseAdapter {
             convertView = inflater.inflate(R.layout.treeview_item, null);
             holder.disclosureImg = (ImageView) convertView.findViewById(R.id.disclosureImg);
             holder.contentText = (TextView) convertView.findViewById(R.id.contentText);
+            holder.bottomView=  convertView.findViewById(R.id.line_bottom);
+            holder.topView=  convertView.findViewById(R.id.line_top);
             convertView.setTag(holder);
         } else {
             holder = (ViewHolder) convertView.getTag();
@@ -81,145 +87,36 @@ public class TreeViewAdapter extends BaseAdapter {
                 holder.disclosureImg.getPaddingBottom());
         holder.contentText.setText(element.getContentText());
         if (element.isHasChildren() && !element.isExpanded()) {
-            //holder.disclosureImg.setImageResource(R.drawable.down_arrow);
-            //Actively set icon visibility here, because convertView may be a reuse of "set invisible" view, the same below.
             holder.disclosureImg.setVisibility(View.VISIBLE);
         } else if (element.isHasChildren() && element.isExpanded()) {
-            //holder.disclosureImg.setImageResource(R.drawable.up_arrow);
             holder.disclosureImg.setVisibility(View.VISIBLE);
         } else if (!element.isHasChildren()) {
-            //holder.disclosureImg.setImageResource(R.drawable.down_arrow);
             holder.disclosureImg.setVisibility(View.INVISIBLE);
-            collapseBrotherNode(element);
             notifyDataSetChanged();
+        }
+        if(element.getLevel() == 0 ){
+            holder.topView.setVisibility(View.VISIBLE);
+            if(element.isExpanded()) {
+            holder.bottomView.setVisibility(View.GONE);}
+            else{
+                holder.bottomView.setVisibility(View.VISIBLE);
+            }
+        }
+        else{
+            holder.topView.setVisibility(View.GONE);
+            holder.bottomView.setVisibility(View.GONE);
         }
         return convertView;
     }
 
     /**
      * Optimizing Holder
-     * @author carrey
      *
+     * @author carrey
      */
-    static class ViewHolder{
+    static class ViewHolder {
         ImageView disclosureImg;
         TextView contentText;
+        View topView,bottomView;
     }
-
-    @NonNull
-    private List<Element> backupDisplayNodes() {
-        List<Element> temp = new ArrayList<>();
-        for (Element displayNode : elementsData) {
-           /* try {
-                temp.add(displayNode.clone());
-            } catch (CloneNotSupportedException e) {
-                temp.add(displayNode);
-            }*/
-        }
-        return temp;
-    }
-
-    public void collapseBrotherNode(Element pNode) {
-        List<Element> temp = backupDisplayNodes();
-        if (pNode.isHasChildren()) {
-            List<Element> roots = new ArrayList<>();
-            for (Element displayNode : elementsData) {
-                if (displayNode.isHasChildren())
-                    roots.add(displayNode);
-            }
-            //Close all root nodes.
-            for (Element root : roots) {
-                if (root.isExpanded() && !root.equals(pNode))
-                    removeChildNodes(root);
-            }
-        }/* else {
-            Element parent = pNode.getParent();
-            if (parent == null)
-                return;
-            List<Element> childList = parent.getChildList();
-            for (Element node : childList) {
-                if (node.equals(pNode) || !node.isExpanded())
-                    continue;
-                removeChildNodes(node);
-            }
-        }*/
-        //notifyDiff(temp);
-    }
-
-    private int removeChildNodes(Element pNode) {
-        return removeChildNodes(pNode, true);
-    }
-
-    private int removeChildNodes(Element pNode, boolean shouldToggle) {
-        if (pNode.isHasChildren())
-            return 0;
-        List<Element> childList = getChildList();
-        int removeChildCount = childList.size();
-        elementsData.removeAll(childList);
-
-       /* if (shouldToggle)
-            pNode.toggle();*/
-        return removeChildCount;
-    }
-
-    public List<Element> getChildList() {
-        return elements;
-    }
-
-
-/*    private void notifyDiff(final List<Element> temp) {
-        DiffUtil.DiffResult diffResult = DiffUtil.calculateDiff(new DiffUtil.Callback() {
-            @Override
-            public int getOldListSize() {
-                return temp.size();
-            }
-
-            @Override
-            public int getNewListSize() {
-                return displayNodes.size();
-            }
-
-            // judge if the same items
-            @Override
-            public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
-                return TreeViewAdapter.this.areItemsTheSame(temp.get(oldItemPosition), displayNodes.get(newItemPosition));
-            }
-
-            // if they are the same items, whether the contents has bean changed.
-            @Override
-            public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
-                return TreeViewAdapter.this.areContentsTheSame(temp.get(oldItemPosition), displayNodes.get(newItemPosition));
-            }
-
-            @Nullable
-            @Override
-            public Object getChangePayload(int oldItemPosition, int newItemPosition) {
-                return TreeViewAdapter.this.getChangePayload(temp.get(oldItemPosition), displayNodes.get(newItemPosition));
-            }
-        });
-        diffResult.dispatchUpdatesTo(this);
-    }
-
-    // For DiffUtil, if they are the same items, whether the contents has bean changed.
-    private boolean areContentsTheSame(Element oldNode, Element newNode) {
-        return oldNode.getContent() != null && oldNode.getContent().equals(newNode.getContent())
-                && oldNode.isExpand() == newNode.isExpand();
-    }
-
-    private Object getChangePayload(Element oldNode, Element newNode) {
-        Bundle diffBundle = new Bundle();
-        if (newNode.isExpanded() != oldNode.isExpanded()) {
-            diffBundle.putBoolean(KEY_IS_EXPAND, newNode.isExpanded());
-        }
-        if (diffBundle.size() == 0)
-            return null;
-        return diffBundle;
-    }
-
-    // judge if the same item for DiffUtil
-    private boolean areItemsTheSame(TreeNode oldNode, TreeNode newNode) {
-        return oldNode.getContent() != null && oldNode.getContent().equals(newNode.getContent());
-    }*/
-
-
 }
--- app/src/main/java/com/murali/customtree/TreeViewItemClickListener.java
+++ app/src/main/java/com/murali/customtree/TreeViewItemClickListener.java
@@ -25,6 +25,7 @@ public class TreeViewItemClickListener implements OnItemClickListener {
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position,
                             long id) {
+
         //Click on the item to represent the element
         Element element = (Element) treeViewAdapter.getItem(position);
         //Elements in trees
@@ -32,36 +33,70 @@ public class TreeViewItemClickListener implements OnItemClickListener {
         //Element data source
         ArrayList<Element> elementsData = treeViewAdapter.getElementsData();
 
+
         //Click on item without subitem to return directly
         if (!element.isHasChildren()) {
             Toast.makeText(view.getContext(),elements.get(position).getContentText() + ":::" + parent.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
+            collapseOthers(element, elements);
+            treeViewAdapter.notifyDataSetChanged();
             return;
         }
 
         if (element.isExpanded()) {
-            element.setExpanded(false);
-            //Delete the corresponding sub-node data within the node, including the sub-node of the sub-node.
-            ArrayList<Element> elementsToDel = new ArrayList<Element>();
-            for (int i = position + 1; i < elements.size(); i++) {
-                if (element.getLevel() >= elements.get(i).getLevel())
-                    break;
-                elementsToDel.add(elements.get(i));
-            }
-            elements.removeAll(elementsToDel);
-            treeViewAdapter.notifyDataSetChanged();
+            collapse(position, element, elements);
         } else {
-            element.setExpanded(true);
-            //Extract the data from the data source and add it to the tree. Note that only the next level of the data is added here to simplify the logic.
-            int i = 1;//Note that the counter here is placed outside of for to ensure that the count is valid.
-            for (Element e : elementsData) {
-                if (e.getParendId() == element.getId()) {
-                    e.setExpanded(false);
-                    elements.add(position + i, e);
-                    i ++;
+            Expend(position, element, elements, elementsData);
+        }
+        treeViewAdapter.notifyDataSetChanged();
+    }
+
+    private void Expend(int position, Element element, ArrayList<Element> elements, ArrayList<Element> elementsData) {
+        //Extract the data from the data source and add it to the tree. Note that only the next level of the data is added here to simplify the logic.
+        element.setExpanded(true);
+        int i = 1;//Note that the counter here is placed outside of for to ensure that the count is valid.
+        for (Element e : elementsData) {
+            if (e.getParendId() == element.getId()) {
+                e.setExpanded(false);
+                elements.add(position + i, e);
+                i ++;
+            }
+        }
+        collapseOthers(element, elements);
+    }
+
+    private void collapseOthers(Element element, ArrayList<Element> elements) {
+        ArrayList<Integer> parents =new ArrayList<>();
+
+        getParents(element.getId(), elements, parents);
+
+        for (int j = 0; j < elements.size(); j++) {
+            if (!parents.contains(elements.get(j).getId())) {
+                    collapse(j, elements.get(j), elements);
                 }
             }
-            treeViewAdapter.notifyDataSetChanged();
+    }
+
+    private void getParents(int id, ArrayList<Element> elements, ArrayList<Integer> parents) {
+
+            for(int j =0;j<elements.size();j++){
+                if(elements.get(j).getId() == id) {
+                    parents.add(elements.get(j).getId());
+                    if(elements.get(j).getParendId() != -1) {
+                        getParents(elements.get(j).getParendId(),elements,parents);
+                    }
+            }
         }
     }
 
+    private void collapse(int position, Element element, ArrayList<Element> elements) {
+        element.setExpanded(false);
+        //Delete the corresponding sub-node data within the node, including the sub-node of the sub-node.
+        ArrayList<Element> elementsToDel = new ArrayList<Element>();
+        for (int i = position + 1; i < elements.size(); i++) {
+            if (element.getLevel() >= elements.get(i).getLevel())
+                break;
+            elementsToDel.add(elements.get(i));
+        }
+        elements.removeAll(elementsToDel);
+    }
 }
