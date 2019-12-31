8f4a2fb3f51d692748a79cd3736e4e3241a38010
muralimohanarao_r <muralimohanarao.ramarao@photoninfotech.net>
2019-12-31 19:41:17 +0530

    Added Readme File & Video

--- app/src/main/java/com/murali/customexpandable/kotlin/TreeViewActivity.kt
+++ app/src/main/java/com/murali/customexpandable/kotlin/TreeViewActivity.kt
@@ -1,9 +1,14 @@
 package com.murali.customexpandable.kotlin
 
 import android.app.Activity
+import android.content.res.AssetManager
 import android.os.Bundle
 import android.support.v7.widget.RecyclerView
+import com.google.gson.Gson
 import com.murali.customexpandable.R
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.io.Reader
 import java.util.*
 
 
@@ -18,6 +23,22 @@ class TreeViewActivity : Activity() {
         setContentView(R.layout.nlevel_activity_main)
 
         mRecyclerView = findViewById(R.id.expList) as RecyclerView;
+    }
+
+    fun loadJsonDataFromAssets() = try {
+        var assetManager: AssetManager = assets
+        var inputStream = assets.open("expand_list.json")
+        var gson :Gson = Gson()
+        var reader : Reader = InputStreamReader(inputStream)
+        var elements: Elements = gson.fromJson(reader,Elements())
+
+//        for (elemntsData in  elements.elementsData.size){
+//            elementsData.add(elemntsData)
+//            if (elemntsData.)
+//        }
+
+    }catch(e : Exception) {
+
     }
 
 }
