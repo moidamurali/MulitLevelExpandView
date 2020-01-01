bf4b64bf9b61cfb00b8c195a164050107644c8cb
Murali <mohanmca009@gamil.com>
2020-01-01 07:17:58 +0530

    Static Data  - Implementation

--- app/src/main/java/com/murali/customexpandable/java/TreeViewActivity.java
+++ app/src/main/java/com/murali/customexpandable/java/TreeViewActivity.java
@@ -37,6 +37,7 @@ public class TreeViewActivity extends Activity {
         LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
         loadDynamicData();
+        //initializeStaticData();
 
         ListView treeview = (ListView) findViewById(R.id.treeview);
         TreeViewAdapter treeViewAdapter = new TreeViewAdapter(parentElements, elementsData, inflater);
@@ -45,6 +46,7 @@ public class TreeViewActivity extends Activity {
         treeview.setOnItemClickListener(treeViewItemClickListener);
     }
 
+    //Load data drom json
     private void loadDynamicData() {
 
         parentElements = new ArrayList<>();
@@ -71,6 +73,65 @@ public class TreeViewActivity extends Activity {
         }catch(IOException e) {
             e.printStackTrace();
         }
+
+    }
+
+
+    private void initializeStaticData(){
+
+        parentElements = new ArrayList<>();
+        elementsData = new ArrayList<>();
+
+        //Add nodes -- node name, node level, node id, parent node id, whether there are children, whether expand
+
+        //Add outermost nodes
+        Elements.ElementsData e1 = new Elements.ElementsData("Andhra Pradesh", Elements.ElementsData.TOP_LEVEL, ""+0, Elements.ElementsData.NO_PARENT, true, false);
+
+        //Adding Layer 1 Nodes
+        Elements.ElementsData e2 = new Elements.ElementsData("VZM District", Elements.ElementsData.TOP_LEVEL + 1, ""+1, e1.getId(), true, false);
+        //Adding Layer 2 Nodes
+        Elements.ElementsData e3 = new Elements.ElementsData("NelliMarla MD", Elements.ElementsData.TOP_LEVEL + 2, ""+2, e2.getId(), true, false);
+        //Adding Layer 3 Nodes
+        Elements.ElementsData e4 = new Elements.ElementsData("Moida", Elements.ElementsData.TOP_LEVEL + 3, ""+3, e3.getId(), false, false);
+
+        //Adding Layer 1 Nodes
+        Elements.ElementsData e5 = new Elements.ElementsData("VZM District", Elements.ElementsData.TOP_LEVEL + 1, ""+4, e1.getId(), true, false);
+        //Adding Layer 2 Nodes
+        Elements.ElementsData e6 = new Elements.ElementsData("Gurla MD", Elements.ElementsData.TOP_LEVEL + 2, ""+5, e5.getId(), true, false);
+        //Adding Layer 3 Nodes
+        Elements.ElementsData e7 = new Elements.ElementsData("Kella", Elements.ElementsData.TOP_LEVEL + 3, ""+6, e6.getId(), false, false);
+
+        //Adding Layer 1 Nodes
+        Elements.ElementsData e8 = new Elements.ElementsData("India", Elements.ElementsData.TOP_LEVEL + 1, ""+7, e1.getId(), false, false);
+
+        //Add outermost nodes
+        Elements.ElementsData e9 = new Elements.ElementsData("India", Elements.ElementsData.TOP_LEVEL, ""+8, Elements.ElementsData.NO_PARENT, true, false);
+        //Adding Layer 1 Nodes
+        Elements.ElementsData e10 = new Elements.ElementsData("Vizag", Elements.ElementsData.TOP_LEVEL + 1, ""+9, e9.getId(), true, false);
+        //Adding Layer 2 Nodes
+        Elements.ElementsData e11 = new Elements.ElementsData("Vishalakshi Nagar", Elements.ElementsData.TOP_LEVEL + 2, ""+10, e10.getId(), true, false);
+        //Adding Layer 3 Nodes
+        Elements.ElementsData e12 = new Elements.ElementsData("Police Barex", Elements.ElementsData.TOP_LEVEL + 3, ""+11, e11.getId(), true, false);
+        //Adding Layer 4 Nodes
+        Elements.ElementsData e13 = new Elements.ElementsData("Police Quarters", Elements.ElementsData.TOP_LEVEL + 4, ""+12, e12.getId(), false, false);
+
+        //Add the initial tree element
+        parentElements.add(e1);
+        parentElements.add(e9);
+        //create data source
+        elementsData.add(e1);
+        elementsData.add(e2);
+        elementsData.add(e3);
+        elementsData.add(e4);
+        elementsData.add(e5);
+        elementsData.add(e6);
+        elementsData.add(e7);
+        elementsData.add(e8);
+        elementsData.add(e9);
+        elementsData.add(e10);
+        elementsData.add(e11);
+        elementsData.add(e12);
+        elementsData.add(e13);
 
     }
 
